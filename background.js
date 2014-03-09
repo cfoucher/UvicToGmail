839d12801e10487a1d8385f764fc7f41699c07d8
cfoucher <conrad@foucher.ca>
2014-03-08 21:57:46 -0800

    Auto Popup

--- background.js
+++ background.js
@@ -4,11 +4,20 @@ if (typeof String.prototype.startsWith != 'function') {
 	};
 }
 
+//Get end of the URL for Comparisons
+function endsWith(str, suffix) {
+	var test = str.indexOf(suffix);
+    return str.indexOf(suffix) !== -1;
+}
+
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
 	var currentUrl = tab.url;
+	var index = endsWith(currentUrl, "CrseSchdDetl");
 	if (currentUrl.startsWith("https://www.uvic.ca/mypage/")) {
-		chrome.pageAction.show(tabId);
+		if(index){
+			chrome.pageAction.show(tabId);
+		}
 	}
 }
 // Listen for any changes to the URL of any tab.
